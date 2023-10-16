4d7383d487e16215104ff5097a1237e1e7976fcd
vicioalex <alex.linea@gmail.com>
2023-10-16 02:28:38 -0500

    imagen de borrador

--- src/app/components/voz.js
+++ src/app/components/voz.js
@@ -18,18 +18,23 @@ export default function Voz() {
       setAltura(`${e.target.scrollHeight}px`)
     }
   }
+  const handleBorrarTexto = () => {
+    setAltura('auto')
+    setInputText('')
+  }
 
   return (
     <>
       <div className="flex flex-col h-screen p-2">
         <textarea
           style={{
-            lineHeight: '0.6',
+            lineHeight: '0.8',
             border: 'none',
             paddingTop: '10px',
-            paddingLeft: '10px',
+            paddingLeft: '5px',
             height: altura,
           }}
+          value={inputText}
           className={`w-full ${myFont.className} font-sans block text-black text-8xl border-none hover:border-none`}
           placeholder="Katherine"
           onChange={handleCambioTexto}
@@ -47,8 +52,8 @@ export default function Voz() {
                   <button onClick={start}>
                     <Image
                       src="/audifono-start.png"
-                      width={50}
-                      height={50}
+                      width={70}
+                      height={70}
                       alt="Picture of the author"
                     />
                   </button>
@@ -57,8 +62,8 @@ export default function Voz() {
                   <button onClick={pause}>
                     <Image
                       src="/audifono-pausa.png"
-                      width={50}
-                      height={50}
+                      width={70}
+                      height={70}
                       alt="Picture of the author"
                     />
                   </button>
@@ -66,6 +71,15 @@ export default function Voz() {
               </div>
             )}
           </Speech>
+          <button onClick={handleBorrarTexto}>
+            <Image
+              className="pl-4"
+              src="/borrador.png"
+              width={85}
+              height={85}
+              alt="Picture of the author"
+            />
+          </button>
         </div>
       </div>
     </>
