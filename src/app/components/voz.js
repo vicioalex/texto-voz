b278c0ba6469f029e2d77b7b863ca6fcc28b2ed5
vicioalex <alex.linea@gmail.com>
2023-10-16 01:11:04 -0500

    Imagenes a los botones

--- src/app/components/voz.js
+++ src/app/components/voz.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Speech from 'react-text-to-speech'
 import localFont from 'next/font/local'
+import Image from 'next/image'
 
 const myFont = localFont({ src: './LearningCurve.ttf' })
 
@@ -10,6 +11,13 @@ export default function Voz() {
   return (
     <>
       <div className="flex flex-col h-screen p-2">
+        <textarea
+          style={{ lineHeight: '0.6', border: 'none', paddingTop: '10px' }}
+          rows="4"
+          className={`w-full ${myFont.className} font-sans block text-black text-9xl border-none`}
+          placeholder="Escribe aqui"
+          onChange={(e) => setInputText(e.target.value)}
+        />
         <div className="flex justify-center items-center ">
           <Speech
             text={`${inputText}`}
@@ -20,22 +28,29 @@ export default function Voz() {
             {({ speechStatus, start, pause }) => (
               <div>
                 {speechStatus !== 'started' && (
-                  <button onClick={start}>Play</button>
+                  <button onClick={start}>
+                    <Image
+                      src="/audifono-start.png"
+                      width={50}
+                      height={50}
+                      alt="Picture of the author"
+                    />
+                  </button>
                 )}
                 {speechStatus === 'started' && (
-                  <button onClick={pause}>Stop</button>
+                  <button onClick={pause}>
+                    <Image
+                      src="/audifono-pausa.png"
+                      width={50}
+                      height={50}
+                      alt="Picture of the author"
+                    />
+                  </button>
                 )}
               </div>
             )}
           </Speech>
         </div>
-        <textarea
-          style={{ lineHeight: '0.7', border: 'none', paddingTop: '10px' }}
-          rows="5"
-          className={`w-full ${myFont.className} font-sans block text-black text-9xl border-none`}
-          placeholder="Escribe aqui"
-          onChange={(e) => setInputText(e.target.value)}
-        />
       </div>
     </>
   )
