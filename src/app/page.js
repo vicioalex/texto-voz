c97c5740ab7496e269b0456240b242760221a0e9
vicioalex <alex.linea@gmail.com>
2024-07-25 09:57:58 -0500

    agregado de link para probar

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ import Voz from './components/voz'
 function Home() {
   return (
     <div className="mx-auto max-w-screen-lg">
+      <a href="https://www.repuestosfc.com/nava/pdfs/document.pdf">
+        Descargar PDF2
+      </a>
       <Voz />
     </div>
   )
